db8b08f509295ea09c8befea815aeadfc2737e3e
brenner <brenner.sardinha@discente.ufg.br>
2025-07-15 09:41:04 -0300

    nova alteracao

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from './App';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Criação do tema padrão (você pode customizar aqui dentro do objeto)
-const theme = extendTheme({});
-
 const queryClient = new QueryClient();
+const theme = extendTheme({}); // cria tema padrão
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
