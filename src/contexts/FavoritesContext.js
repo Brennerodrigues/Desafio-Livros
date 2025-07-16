e3ebaa1196b6c00ac689fe4481155c7a952012f6
Brennerodrigues <166179540+Brennerodrigues@users.noreply.github.com>
2025-07-16 10:35:19 -0300

    Update FavoritesContext.js
    comentando o codigo

--- src/contexts/FavoritesContext.js
+++ src/contexts/FavoritesContext.js
@@ -1,39 +1,39 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-
+//crio um contexto
 const FavoritesContext = createContext();
-
+//função para acessar favoritos salvos
 export function FavoritesProvider({ children }) {
   const [favorites, setFavorites] = useState(() => {
     const stored = localStorage.getItem('favorites');
     return stored ? JSON.parse(stored) : [];
   });
-
+//Salva a lista de favoritos a cada mudança
   useEffect(() => {
     localStorage.setItem('favorites', JSON.stringify(favorites));
   }, [favorites]);
-
+//remove e adiciona favoritos
   const toggleFavorite = (book) => {
     setFavorites((prev) => {
       const exists = prev.find((item) => item.key === book.key);
       if (exists) {
-        return prev.filter((item) => item.key !== book.key);
+        return prev.filter((item) => item.key !== book.key); //remove
       } else {
-        return [...prev, book];
+        return [...prev, book]; //adiciona
       }
     });
   };
-
-  const isFavorite = (bookKey) => {
+//verifica o livro
+  const isFavorite = (bookKey) => { //retorna true se estiver nos favoritos
     return favorites.some((item) => item.key === bookKey);
   };
-
+//disponibiliza as funções
   return (
     <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite }}>
       {children}
     </FavoritesContext.Provider>
   );
 }
-
+//atalho de acesso
 export function useFavorites() {
   return useContext(FavoritesContext);
 }
